e074842a8440a1bea9b7e6a31809777b09d4ddef
AmberOrtiz <amberoortiz@gmail.com>
2024-06-14 09:24:07 -0700

    all file add

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*============typing animation==============*/
+var typed = new Typed(".typing",{
+    Strings:["Web Designer","Web Developer","Graphic Designer","YouTuber"],
+    typeSpeed:120,
+    BackSpeed:1000,
+    loop:true
+})
